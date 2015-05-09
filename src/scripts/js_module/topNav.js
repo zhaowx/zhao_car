10af8e101aa69cbaa38da209ddf56cb6a56a52e2
luo mingzhong <lmzpengpai@gmail.com>
2015-05-09 20:48:09 +0800

    更新topNav.js 模版，针对登录状态不同显示不同的下拉列表

--- src/scripts/js_module/topNav.js
+++ src/scripts/js_module/topNav.js
@@ -3,6 +3,18 @@
  */
 
 (function(){
+    //如果cookie存在;表示登录，其他表示未登录。
+    var data={
+        isLogin:true//默认没有登录
+    };
+
+    /**
+     * @discription 通过cookie来判断是否登录
+     *
+     * */
+    function initData(){
+        //todo
+    }
     function renderCont() {
         var js_html = ' <div class="container">\
         <div class="navbar-header">\
@@ -27,7 +39,7 @@
                      <li class="dropdown">\
                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">服务承诺<span class="caret"></span></a>\
                          <ul class="dropdown-menu" role="menu">\
-                             <li><a href="#">购车流程</a></li>\
+                             <li><a href="#" class="active">购车流程</a></li>\
                              <li><a href="#">保险服务</a></li>\
                              <li><a href="#">质量保证</a></li>\
                              <li class="divider"></li>\
@@ -37,21 +49,28 @@
                      <li class="dropdown">\
                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">个人中心 <span class="caret"></span></a>\
                          <ul class="dropdown-menu" role="menu">\
-                             <li><a href="#">登陆</a></li>\
+                         {{if isLogin}}\
+                         <li><a href="userIndent.html">我的订单</a></li>\
+                         <li><a href="userPostBuyCar.html">我的求购</a></li>\
+                         <li><a href="user_Vehicle_Information.html">我的车源</a></li>\
+                         <li><a href="userInfomation.html">账户信息</a></li>\
+                         <li><a href="userService.html">投诉建议</a></li>\
+                         <li><a href="userModifyPassword.html">修改密码</a></li>\
+                         <li><a href="void()">退出</a></li>\
+                         {{else}}\
+                            <li><a href="#">登陆</a></li>\
                              <li><a href="#">注册</a></li>\
+                         {{/if}}\
                          </ul>\
                      </li>\
                  </ul>\
              </div>\
          </div>';
-
+        initData();
         var render = template.compile(js_html);
-        var html = render();
+        var html = render(data);
         document.getElementById('js_topnav').innerHTML = html;
 
     }
-
-
     renderCont();
-
 })();
