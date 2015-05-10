6839c4281f928772fdfa2c50daeb1257bd5214b1
weixiong.zhao <weixiong.zhao@qunar.com>
2015-05-10 15:01:25 +0800

    请求url 统一使用var  globalVar  =  window._globalV;

--- src/scripts/page/userPublishWantCar/publish.js
+++ src/scripts/page/userPublishWantCar/publish.js
@@ -1,3 +1,43 @@
 /**
  * Created by weixiong.zhao on 2015/5/10.
  */
+
+(function(){
+    var  globalVar  =  window._globalV;
+    var that = {
+        dataParams: {
+
+        }
+    }
+
+    function communicationSet(){
+        $(document).bind('changeDPData',function(event,data){
+            console.log(data);
+            that.dataParams.pageIndex = 0;
+            var d = $.extend({},data,that.dataParams);
+            that.dataParams = d;
+        })
+        $(document).bind('changeColorData',function(event,data){
+            console.log(data);
+            that.dataParams.pageIndex = 0;
+            var d = $.extend({},data,that.dataParams);
+            that.dataParams = d;
+        })
+    }
+
+    function bindEvents(){
+
+    }
+
+    function init(){
+        if(!$('#js_form')){
+            return;
+        }
+        bindEvents();
+        communicationSet();
+    }
+
+    init();
+
+
+})()
