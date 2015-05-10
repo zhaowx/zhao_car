1f4ff95905329c78c27eb104070c10f584403386
luo mingzhong <lmzpengpai@gmail.com>
2015-05-10 11:05:20 +0800

    在Global中添加了设置，删除,获取 cookie 的 函数

--- src/scripts/page/Global.js
+++ src/scripts/page/Global.js
@@ -2,13 +2,45 @@
  * Created by weixiong.zhao on 2015/5/4.
  * 作为全局通用变量使用
  */
-(function($){
+(function ($) {
     //车系等基本配置表
     var GlobalVar = {
-        carSource:{}
+        carSource: {}
     };
 
 
-
-
 })(jQuery);
+
+/**
+ * @discription 获取cookie的值，如果没有返回null
+ * @param cookieName {string} cookie的名字
+ * */
+function getCookie(cookieName) {
+    var arr = document.cookie.split(';');
+
+    for (var i = 0; i < arr.length; i++) {
+        /* 将cookie名称和值拆分进行判断 */
+        var arr2 = arr[i].split('=');
+        if (arr2[0] == name) {
+            return arr2[1];
+        }
+    }
+    return null;
+}
+
+function setCookie(name, value, iDay) {
+    if (iDay) {
+        var oDate = new Date();
+        oDate.setDate(oDate.getDate() + iDay);
+        document.cookie = name + '=' + value + ';expires=' + oDate;
+    }
+    else
+        document.cookie = name + '=' + value;
+}
+
+/**
+ * @discription 删除cookie
+ * */
+function deleteCookie(cookieName) {
+    setCookie(cookieName, 1, -1);
+}
