7f77a25b2e6bd6b7dffc9da8148244dce8f6dea1
luo mingzhong <lmzpengpai@gmail.com>
2015-05-13 00:46:57 +0800

    编写用户资料

--- src/scripts/page/Global.js
+++ src/scripts/page/Global.js
@@ -16,12 +16,12 @@
  * */
 function getCookie(c_name) {
     if (document.cookie.length > 0) {//先查询cookie是否为空，为空就return ""
-        var c_start = document.cookie.indexOf(c_name + "=")//通过String对象的indexOf()来检查这个cookie是否存在，不存在就为 -1　　
+        var c_start = document.cookie.indexOf(c_name + "=");//通过String对象的indexOf()来检查这个cookie是否存在，不存在就为 -1　　
         if (c_start != -1) {
-            c_start = c_start + c_name.length + 1//最后这个+1其实就是表示"="号啦，这样就获取到了cookie值的开始位置
-            var c_end = document.cookie.indexOf(";", c_start)//其实我刚看见indexOf()第二个参数的时候猛然有点晕，后来想起来表示指定的开始索引的位置...这句是为了得到值的结束位置。因为需要考虑是否是最后一项，所以通过";"号是否存在来判断
-            if (c_end == -1) c_end = document.cookie.length
-            return unescape(document.cookie.substring(c_start, c_end))//通过substring()得到了值。想了解unescape()得先知道escape()是做什么的，都是很重要的基础，想了解的可以搜索下，在文章结尾处也会进行讲解cookie编码细节
+            c_start = c_start + c_name.length + 1;//最后这个+1其实就是表示"="号啦，这样就获取到了cookie值的开始位置
+            var c_end = document.cookie.indexOf(";", c_start);//其实我刚看见indexOf()第二个参数的时候猛然有点晕，后来想起来表示指定的开始索引的位置...这句是为了得到值的结束位置。因为需要考虑是否是最后一项，所以通过";"号是否存在来判断
+            if (c_end == -1) c_end = document.cookie.length;
+            return unescape(document.cookie.substring(c_start, c_end));//通过substring()得到了值。想了解unescape()得先知道escape()是做什么的，都是很重要的基础，想了解的可以搜索下，在文章结尾处也会进行讲解cookie编码细节
         }
     }
     return null;
@@ -66,36 +66,19 @@ function getFileName() {
     return fileName[0];
 }
 function isInUserCenter() {
-    var result = false;
-    switch (getFileName()) {
-        case "userCar":
-            result = true;
-            break;
-        case "userIndent":
-            result = true;
-            break;
-        case "userInfomation":
-            result = true;
-            break;
-        case "userModifyPassword":
-            result = true;
-            break;
-        case "usrService":
-            result = true;
-            break;
-        case "userWant":
-            result = true;
-            break;
-        case "userPublishCar":
-            result = true;
-            break;
-        case "userPublishWantCar":
+    var _fileName = getFileName(),
+        result = false;
+    if (/^user*/.test(_fileName)) {
+        _fileName = "userCenter";
+    }
+
+    switch (_fileName) {
+        case "userCenter":
             result = true;
             break;
         default:
             result = false;
             break;
-
     }
     return result;
 }
