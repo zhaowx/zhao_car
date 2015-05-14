8284e772ba34920cdf8bb34d941d5a0d10227e39
luo mingzhong <lmzpengpai@gmail.com>
2015-05-15 00:10:39 +0800

    完成静态页面

--- src/scripts/page/userInfomation/userINfomation.js
+++ src/scripts/page/userInfomation/userINfomation.js
@@ -1,3 +1,23 @@
 /**
  * Created by luomingzhong on 2015/5/12.
  */
+(function () {
+    'use strict';
+    var token = getCookie('token');
+    $.ajax({
+        url: window._globalV.reqUrl,//·¢ËÍµÄµØÖ·
+        data: {
+            cmd: 10027,
+            token:token,
+            dataPacket: {
+            }
+        },
+        timeout: window._globalV.ajaxTimeOut,
+        dataType: 'jsonp',
+        type: 'GET'
+    }).done(function (data) {
+        console.log(data)
+    }).fail(function (data) {
+       alert("Somthing is error");
+    });
+}());
